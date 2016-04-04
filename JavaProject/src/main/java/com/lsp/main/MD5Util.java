ade35dfe9f7076ac043ca867674e081e25856cd0
liusiping30@qq.com <liusiping30@qq.com>
2016-04-05 00:43:43 +0800

    Base64，MD5，SHA

---
+++ JavaProject/src/main/java/com/lsp/main/MD5Util.java
@@ -0,0 +1,65 @@
+package com.lsp.main;
+import java.security.MessageDigest;
+
+public class MD5Util {
+    public final static String encode(String s) {
+        char hexDigits[]={'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};       
+        try {
+            byte[] btInput = s.getBytes();
+            // 获得MD5摘要算法的 MessageDigest 对象
+            MessageDigest mdInst = MessageDigest.getInstance("MD5");
+            // 使用指定的字节更新摘要
+            mdInst.update(btInput);
+            // 获得密文
+            byte[] md = mdInst.digest();
+            // 把密文转换成十六进制的字符串形式
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    /*** 
+     * MD5加密 生成32位md5码
+     * @param 待加密字符串
+     * @return 返回32位md5码
+     */
+    public static String md5Encode(String inStr) throws Exception {
+        MessageDigest md5 = null;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            e.printStackTrace();
+            return "";
+        }
+
+        byte[] byteArray = inStr.getBytes("UTF-8");
+        byte[] md5Bytes = md5.digest(byteArray);
+        StringBuffer hexValue = new StringBuffer();
+        for (int i = 0; i < md5Bytes.length; i++) {
+            int val = ((int) md5Bytes[i]) & 0xff;
+            if (val < 16) {
+                hexValue.append("0");
+            }
+            hexValue.append(Integer.toHexString(val));
+        }
+        return hexValue.toString();
+    }
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println(MD5Util.encode("20121221"));
+        System.out.println(MD5Util.md5Encode("20121221"));
+        System.out.println(MD5Util.encode("加密"));
+        System.out.println(MD5Util.md5Encode("加密"));
+    }
+}
